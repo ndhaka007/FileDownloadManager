f64d622124b399397a4038d6944d00a142501821
ndhaka007 <nikhil.dhaka.bme14@itbhu.ac.in>
2019-08-20 18:07:28 +0530

    method added

--- Controller/Controller.go
+++ Controller/Controller.go
@@ -17,65 +17,71 @@ var (
 
 //API to check health
 func HomePage(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Ok","200")
-	_, _ = fmt.Fprintf(w, "Ok")
+	if r.Method =="GET" {
+		w.Header().Set("Ok", "200")
+		_, _ = fmt.Fprintf(w, "Ok")
+	}
 }
 
 //API to download files
 func Download(w http.ResponseWriter, r *http.Request) {
-	//to generate uuid for request
-	uuid := generateUUID()
-	//map to store url and there respective downloaded images
-	urlVsAdd := make(map[string]string)
-	//request time
-	startTime :=time.Now()
-	//read payload
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	//convert to json
-	var file Model.Input
-	_ = json.Unmarshal(reqBody, &file)
-	if file.Type == "serial" {
-		//serial download
-		se := Model.SerialDownload{Urls: file.Urls}
-		e := se.DownloadFile(urlVsAdd,uuid)
+	if r.Method =="POST" {
+		//to generate uuid for request
+		uuid := generateUUID()
+		//map to store url and there respective downloaded images
+		urlVsAdd := make(map[string]string)
+		//request time
+		startTime := time.Now()
+		//read payload
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		//convert to json
+		var file Model.Input
+		_ = json.Unmarshal(reqBody, &file)
+		if file.Type == "serial" {
+			//serial download
+			se := Model.SerialDownload{Urls: file.Urls}
+			e := se.DownloadFile(urlVsAdd, uuid)
 
-		//save the data in response and map and return the id
-		resp := &Model.Response{Id: uuid, StartTime: startTime, EndTime: time.Now(), Status: "successful", DownloadType: file.Type, Files: urlVsAdd}
-		if e != nil {
-			resp.Status = "failed"
-		}
-		w.Header().Set("Content-Type", "application/json")
-		ret,_:=json.Marshal(resp.Id)
-		Mp[uuid] = resp
-		_, _ = w.Write(ret)
-	}	else{
-		//concurrent download
-		//save the data in response and map and return the id
-		resp := &Model.Response{Id: uuid, StartTime: startTime, EndTime: time.Now(), Status: "queue", DownloadType: file.Type, Files: urlVsAdd}
-		w.Header().Set("Content-Type", "application/json")
-		ret,_:=json.Marshal(resp.Id)
-		Mp[uuid] = resp
-		_, _ = w.Write(ret)
+			//save the data in response and map and return the id
+			resp := &Model.Response{Id: uuid, StartTime: startTime, EndTime: time.Now(), Status: "successful", DownloadType: file.Type, Files: urlVsAdd}
+			if e != nil {
+				resp.Status = "failed"
+			}
+			w.Header().Set("Content-Type", "application/json")
+			ret, _ := json.Marshal(resp.Id)
+			Mp[uuid] = resp
+			_, _ = w.Write(ret)
+		} else {
+			//concurrent download
+			//save the data in response and map and return the id
+			resp := &Model.Response{Id: uuid, StartTime: startTime, EndTime: time.Now(), Status: "queue", DownloadType: file.Type, Files: urlVsAdd}
+			w.Header().Set("Content-Type", "application/json")
+			ret, _ := json.Marshal(resp.Id)
+			Mp[uuid] = resp
+			_, _ = w.Write(ret)
 
-		 co := Model.ConDownload{Urls: file.Urls}
-		 e := co.DownloadFile(urlVsAdd,uuid,resp)
-		if e != nil {
-			resp.Status = "failed"
+			co := Model.ConDownload{Urls: file.Urls}
+			e := co.DownloadFile(urlVsAdd, uuid, resp)
+			if e != nil {
+				resp.Status = "failed"
+			}
+			Mp[uuid] = resp
 		}
-		Mp[uuid] = resp
 	}
 }
 
 //API to check status
 func Status(w http.ResponseWriter, r *http.Request){
-	id := parseURL(r.URL.Path)
-	if _,va := Mp[id]; !va{
-		w.Header().Set("id not found","404")
-		_, _ = fmt.Fprintf(w, "404 Id not Found")
-	} else{
-	w.Header().Set("Content-Type", "application/json")
-	ret,_:=json.Marshal(Mp[id])
-		_, _ = w.Write(ret)
+	if r.Method =="GET" {
+		id := parseURL(r.URL.Path)
+		if _, va := Mp[id]; !va {
+			w.Header().Set("id not found", "404")
+			_, _ = fmt.Fprintf(w, "404 Id not Found")
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			ret, _ := json.Marshal(Mp[id])
+			_, _ = w.Write(ret)
+		}
 	}
 }
 
